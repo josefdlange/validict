8fb56338cd3aae382b0b44521c0736c961bf36af
Josef Lange <josef@loupapp.com>
2015-05-08 12:07:01 -0700

    Initial commit of 1.0 code with passing tests.

---
+++ tests/core_tests.py
@@ -0,0 +1,75 @@
+import unittest
+
+from validict import validate, FailedValidationError
+
+class ValidictTests(unittest.TestCase):
+    def test_good_dict(self):
+        template = {
+            'name': str,
+            'age': int,
+            'pets': [
+                {
+                    'name': str,
+                    'kind': str
+                }
+            ],
+            'parents': ([{'name': str}], int, None)
+        }
+        
+        kid = {
+            'name': "Bart Simpson",
+            'age': 10,
+            'pets': [
+                {'name': "Santa's Little Helper", 'kind': "Dog"},
+                {'name': "Snowball II", 'kind': "Cat"}
+            ],
+            'parents': [
+                {'name': "Homer Simpson"},
+                {'name': "Marge Simpson"}
+            ]
+        }
+        
+        self.assertTrue(validate(template, kid))
+
+    
+    def test_bad_dict_loud(self):
+        template = {
+            'name': str,
+            'age': int,
+            'pets': [
+                {
+                    'name': str,
+                    'kind': str
+                }
+            ],
+            'parents': ([{'name': str}], int, None)
+        }
+
+        bad_kid = {
+            'name': "Nelson Muntz",
+            'age': 12
+        }
+
+        with self.assertRaises(FailedValidationError):
+            validate(template, bad_kid)  
+
+    
+    def test_bad_dict_quiet(self):
+        template = {
+            'name': str,
+            'age': int,
+            'pets': [
+                {
+                    'name': str,
+                    'kind': str
+                }
+            ],
+            'parents': ([{'name': str}], int, None)
+        }
+ 
+        bad_kid = {
+            'name': "Nelson Muntz",
+            'age': 12
+        }
+
+        self.assertFalse(validate(template, bad_kid, quiet=True))
---
+++ validict/__init__.py
@@ -0,0 +1,57 @@
+def validate(template, unvalidated, quiet=False):
+    try:
+        if isinstance(template, tuple):
+            # We have multiple options on the template level.
+            valid = False
+            for template_option in template:
+                try:
+                    valid = validate(template_option, unvalidated)
+                    if valid:
+                        break
+                except FailedValidationError:
+                    pass
+            if valid:
+                return True
+            else:
+                raise FailedValidationError("None of {0} in template match topmost level of {1}".format(template, unvalidated))
+
+        if isinstance(template, dict) and isinstance(unvalidated, dict):
+            # Two dictionaries. Compare key-by-key!
+            try:
+                if all([validate(template[key], unvalidated[key]) for key in template]):
+                    return True
+                else:
+                    raise FailedValidationError("{0} in template did not match topmost level of {1}".format(template, unvalidated))
+            except KeyError:
+                # Unvalidated didn't have the key. FAIL!
+                raise FailedValidationError("Key {0} was not found in the topmost level of {1}".format(key, unvalidated))
+
+        elif isinstance(template, list) and isinstance(unvalidated, list):
+            # Two lists. The template list should have one element to demonstrate its members'
+            # structure. This can be a tuple.
+            if all([validate(template[0], item) for item in unvalidated]):
+                return True
+            else:
+                raise FailedValidationError("Not all list items in {0} matched template {1}".format(unvalidated, template))
+
+        elif isinstance(template, type):
+            # Template declared a type. Time to compare values.
+            if isinstance(unvalidated, template):
+                return True
+            else:
+                raise FailedValidationError("{0} is not of type {1}".format(unvalidated, template))
+
+        else:
+            if template == unvalidated:
+                return True
+            else:
+                raise FailedValidationError("{0} is not equal to {1}".format(unvalidated, template))
+    except FailedValidationError, e:
+        if quiet:
+            return False
+        else:
+            raise e
+
+
+class FailedValidationError(Exception):
+    pass
