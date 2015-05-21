a440c71d80bf85f1416b2bafa47b48e6aaeb7121
Josef Lange <josef@loupapp.com>
2015-05-20 18:54:31 -0700

    Bumping version number in setup.py to 1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
         name = "validict",
-        version = "1.3",
+        version = "1.4",
         author = "Josef Lange",
         author_email = "josef.d.lange@me.com",
         description = "A simple validation module",
