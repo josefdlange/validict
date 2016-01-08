2b0493742b8da82f0318feeac2d8291c226d29b9
sgs00 <g.saviano@gmail.com>
2016-01-08 12:07:44 +0100

    indentation

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-        name="validict",
-        version="1.4",
-        author="Josef Lange",
-        author_email="josef.d.lange@me.com",
-        description="A simple validation module",
-        license="MIT",
-        keywords="validation dictionary validate valid list json request validator",
-        url="https://github.com/josefdlange/validict",
-        packages=find_packages(),
-        test_suite='tests.core_tests',
-        long_description=read('README.rst'),
-        classifiers=[
-            'Topic :: Utilities',
-            'License :: OSI Approved :: MIT License'
-            ]
-        )
+    name="validict",
+    version="1.4",
+    author="Josef Lange",
+    author_email="josef.d.lange@me.com",
+    description="A simple validation module",
+    license="MIT",
+    keywords="validation dictionary validate valid list json request validator",
+    url="https://github.com/josefdlange/validict",
+    packages=find_packages(),
+    test_suite='tests.core_tests',
+    long_description=read('README.rst'),
+    classifiers=[
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
